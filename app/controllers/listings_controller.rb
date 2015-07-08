77ab87a2339b7af8dd240cb53d40e78d9e5b496a
kha & chase <student@epicodus.com>
2015-07-08 16:01:44 -0700

    will allow user to add listings to individual listings page.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,4 +3,24 @@ class ListingsController < ApplicationController
     @user = User.find(params[:user_id])
     @listings = Listing.all()
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @listing = Listing.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @listing = @user.listings.new(listing_params)
+    if @listing.save
+      redirect_to user_listings_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:address, :listingtype, :price)
+  end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,7 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
+
+  validates_presence_of :address
+  validates_presence_of :listingtype
+  validates_presence_of :price
 end
